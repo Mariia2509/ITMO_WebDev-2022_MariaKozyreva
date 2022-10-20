1de6d96553be8a68fd1bf3239740e99a683ffeeb
Mariia2509 <maria2509@list.ru>
2022-10-20 11:36:09 +0300

    Lessons/lesson10-method

---
+++ Lesson9-list-TODO-LocalStorage/src/model/vos/TodoVO.js
@@ -0,0 +1,15 @@
+class TodoVO {
+  static createTodoVO(title) {
+    const todoId = Date.now().toString();
+    return new TodoVO(todoId, title);
+  }
+
+  constructor(id, title, date = new Date()) {
+    this.id = id;
+    this.title = title;
+    this.data = date;
+    this.completed = false;
+  }
+}
+
+export default TodoVO;
