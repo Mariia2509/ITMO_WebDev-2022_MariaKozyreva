0abbcb1fe5e9a0120660d01677013d6d0bde4e79
Mariia2509 <maria2509@list.ru>
2022-10-27 22:14:37 +0300

    Область видимости_Замыкания.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 // vite.config.js
+import * as path from "path";
+
 export default {
   // config options
+
+
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
 
   server: {
     port: 8888,
   },
 };
+
