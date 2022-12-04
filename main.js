673f92eff0f91cf35e4cf331b6f3094f5cfce3b7
Maria <maria2509@list.ru>
2022-12-04 19:54:03 +0300

    Добавить обработчики событий.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hello');
+import { handleNumberInput, handleIbanInput, handleAddButtonClick } from './src/js/mainPageHandlers'
+
+function init () {
+  const invoiceInput = document.getElementById("number-invoice-input")
+  const ibanInput = document.getElementById("number-iban-input")
+  const addButton = document.getElementById("add-button")
+
+  invoiceInput.addEventListener("input", handleNumberInput)
+  ibanInput.addEventListener("input", handleIbanInput)
+  addButton.addEventListener("click", handleAddButtonClick)
+}
+
+init()
---
+++ src/js/mainPageHandlers.js
@@ -0,0 +1,13 @@
+
+
+export function handleNumberInput () {
+  console.log("hi")
+}
+
+export function handleAddButtonClick () {
+  console.log("handleAddButtonClick")
+}
+
+export function handleIbanInput () {
+  console.log("handleIbanInput")
+}
