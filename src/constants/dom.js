0b679d9bd41f5cac2c898d65072e774c50ea6f9c
Mariia2509 <maria2509@list.ru>
2022-12-08 22:33:55 +0300

    save1

--- src/constants/dom.js
+++
@@ -1,8 +0,0 @@
-class DOM {
-  static BTN_CREATE_TODO = 'btnCreateTodo';
-  static LIST_OF_TODOS = 'listOfTodos';
-  static INP_TODO_TITLE = 'inpTodoTitle';
-  static APP = 'app';
-}
-
-export default DOM;
