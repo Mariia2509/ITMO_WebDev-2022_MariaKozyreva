d4eb85b228d5df56b2cfeab50ef1e11505c43a8d
Mariia2509 <maria2509@list.ru>
2022-12-10 20:00:37 +0300

    Создать Counter1

--- src/AppCounter.vue
+++ src/AppCounter.vue
@@ -1,28 +1,43 @@
 <template>
   <h1>App Counter</h1>
-  <p v-for='i in [1,2,3]' class='counter-value' :key='i'>{{counter}}
-  </p>
-  <button v-on:click='counter++'>+</button>
-  <button v-if='counter > 0' @click='counter--'>-</button>
+    <CounterValue v-for="fruits in [{ index:1, text:'Clicked'},]" :title='fruits.text' :value ='counter' :key='fruits.index'/>
+
+  <button v-on:click="onPlus">+</button>
+  <button v-if='canRenderMinusVisible' @click='onMinus'>-</button>
 </template>
 <script>
+import CounterValue from '@/components/CounterValue.vue';
+
 export default {
+  components: {
+    CounterValue,
+  },
   data() {
     return{
       counter: 0,
-      innerHTML: `<h2>Hello</h2>`
     };
   },
-
+  computed: {
+    canRenderMinusVisible(){
+      return this.counter>0
+    }
+  },
+  methods: {
+    onPlus(){
+      this.counter++;
+    },
+    onMinus(){
+      this.counter--
+    }
+  }
 
 }
 </script>
-<style scoped>
- p.counter-value{
-   color: red;
-   font-size: 2rem;
-   font-weight: normal;
- }
-</style>
-
+<!--<style scoped>-->
+<!-- p.value{-->
+<!--   color: red;-->
+<!--   font-size: 2rem;-->
+<!--   font-weight: normal;-->
+<!-- }-->
+<!--</style>-->
 
