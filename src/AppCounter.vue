40e7a2055aceffe4a1f04b4648c6439a08262bab
Mariia2509 <maria2509@list.ru>
2022-12-12 01:15:12 +0300

    1.Сохранять в localStorage. 2.Создать кнопки "добавить" и "удалить"

--- src/AppCounter.vue
+++ src/AppCounter.vue
@@ -1,17 +1,31 @@
 <template>
-  <h1 ref='header'>App Counter</h1>
-    <CounterValue class='counter'
-                  v-for="fruits in [{ index:1, text:'Clicked'},]" :title='fruits.text' :value ='counter' :key='fruits.index'/>
-
+  <h1 ref="header">App Counter</h1>
+  <input v-model="inputText" />
+  <button @click="onButtonClick" :disabled="isAllowedToSave">Add</button>
+  <h2 v-for="(todo, index) in list" :key="index">
+    {{ todo }}
+    <button @click="onDelete(index)">x</button>
+  </h2>
+  <CounterValue
+    v-if="isShown"
+    class="counter"
+    v-for="obj in [{ index: 1, text: 'Clicked' }]"
+    :title="obj.text"
+    :value="counter"
+    :key="obj.index"
+  />
   <button v-on:click="onPlus">+</button>
-  <button v-if='canRenderMinusVisible' @click='onMinus'>-</button>
+  <button v-if="canRenderMinusButton" @click="onMinus">-</button>
 </template>
 <script>
-import CounterValue from '@/components/CounterValue.vue';
+import CounterValue from './components/CounterValue.vue';
 
-const LOCAL_KEY_COUNTER = 'counter'
+const LOCAL_KEY_COUNTER = 'counter';
+const LOCAL_KEY_TEXT = 'text';
+const LOCAL_KEY_LIST = 'list';
 
-const saveCounter = (value) => localStorage.setItem(LOCAL_KEY_COUNTER, value)
+const save = (key, value) => localStorage.setItem(key, value);
+
 let counterWatcher = null;
 
 export default {
@@ -21,51 +35,77 @@ export default {
   data() {
     return {
       counter: 0,
+      inputText: '',
+      list: [],
     };
   },
   created() {
-    console.log('>created: ', this.counter);
-    this.counter = localStorage.getItem('counter')||0;
+    console.log('> created: ', this.counter);
+    this.list = JSON.parse(localStorage.getItem(LOCAL_KEY_LIST)) || [];
+    this.inputText = localStorage.getItem(LOCAL_KEY_TEXT) || '';
+    this.counter = localStorage.getItem(LOCAL_KEY_COUNTER) || 0;
     counterWatcher = this.$watch(
-      ()=>this.counter,
-      (oldValue, newValue)=>{
-        saveCounter(newValue)
+      () => this.counter,
+      (newValue, oldValue) => {
+        console.log('> counter watched:', { newValue, oldValue });
+        save(LOCAL_KEY_COUNTER, newValue);
+      },
+    );
+    this.$watch(
+      () => this.inputText,
+      (value) => {
+        save(LOCAL_KEY_TEXT, value);
+      },
+    );
+    this.$watch(
+      () => this.list.length,
+      (value) => {
+        console.log('> watch: list =', value);
+        save(LOCAL_KEY_LIST, JSON.stringify(this.list));
       },
     );
   },
   mounted() {
-    console.log('>mounted: ', this.counter);
+    console.log('> mounted: ', this.counter);
   },
   computed: {
-canRenderMinusVisible() {
-  return this.counter > 0
-},
-},
-  methods: {
-    onPlus(){
-      this.counter++;
-      // if(this.counter > 11) this.$destroy();
-      console.log('> Counter -> onPlus:', this.counter);
+    isShown() {
+      return this.counter < 15;
     },
-    onMinus(){
-      this.counter--
-      if (this.counter===0) {
-        this.$refs.header.innerHTML = '<b>Header</b>'
+    canRenderMinusButton() {
+      return this.counter > 0;
+    },
+    isAllowedToSave() {
+      return this.inputText.length === 0;
+    },
+  },
+  methods: {
+    onDelete(index) {
+      this.list.splice(index, 1);
+    },
+    onButtonClick() {
+      this.list.push(this.inputText);
+      this.inputText = '';
+    },
+    onPlus() {
+      this.counter++;
+      console.log('> Counter -> onPlus:', this.counter, this);
+    },
+    onMinus() {
+      this.counter--;
+      if (this.counter === 0) {
+        this.$refs.header.innerText = `Header: ${this.counter}`;
       }
-      console.log('> Counter -> onMinus:', this.counter)
-    }
+      console.log('> Counter -> onMinus:', this.counter);
+    },
   },
   unmounted() {
     counterWatcher();
   },
-}
+};
 </script>
-<style lang='scss' scoped>
- .counter {
-   color: green;
- }
- .alert{
-   background-color: lightcoral;
- }
+<style lang="scss" scoped>
+.counter {
+  color: green;
+}
 </style>
-
