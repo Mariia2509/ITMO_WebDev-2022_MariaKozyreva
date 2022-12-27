49d5344f7e6bcfdcc38990aad64ce0023c000c61
Mariia2509 <maria2509@list.ru>
2022-12-27 23:26:20 +0300

    save_login-register

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import PocketBase from 'pocketbase';
+import './style.css';
+import Routes from './consts/Routes';
+import router from './router';
+import App from './App.vue';
 
-const db = new PouchDB('http://90.156.224.65:5984/books');
+const pb = new PocketBase(import.meta.env.VITE_DATABASE_URL);
+
+console.log('> pb.authStore.isValid:', pb.authStore.isValid);
+
+router.beforeEach((to, from, next) => {
+  const authRoutes = [Routes.INDEX, Routes.LOGIN, Routes.REGISTER];
+  const indexOfAuthRoute = authRoutes.indexOf(to.path);
+  if (indexOfAuthRoute < 0 && !pb.authStore.isValid) {
+    next({ path: Routes.LOGIN });
+  } else next();
+});
+
 const app = createApp(App);
-app.provide('db', db);
+app.use(router);
+app.provide('pb', pb);
 app.mount('#app');
