d3080d3d95a3426c05583015f18ee363e091baa8
Mariia2509 <maria2509@list.ru>
2023-01-30 17:28:36 +0300

    save1

--- app.vue
+++ app.vue
@@ -1,6 +1,18 @@
+<script setup>
+const setting useRuntimeConfig();
+await useFetch(`${setting.DATA_URL}/https://jsonplaceholder.typicode.com/users/1`)
+</script>
+
+
+
+
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <NuxtPage :transition="{
+          name: 'slide-left',
+          mode: 'out-in'
+              }" />
+
   </NuxtLayout>
 </template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: false,
+    vite:{
 
+    },
+
+        runtimeConfig: {
+            public:
+            DATA_API: https:jsonplaceholder.typicode.com
+            }
+        },
+            app: {
+            pageTransition: { name: 'page', mode: 'out-in' }
+},
 })
+
+
+
+    })
+}
